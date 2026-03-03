d209c51c029035a8805aca6bb9ee3b6eedfc6155
Đào Nhật Tân <74749243+tiin1705@users.noreply.github.com>
2026-03-03 16:30:25 +0700

    Tạo Player core: Add card cho player, play card

---
+++ Assets/Project/Scripts/Core/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uno3D.Core
+{
+    public class Player
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+
+        private List<Card> _hand = new List<Card>();
+        //Xự kiện thông báo khi bài trên tay thay đổi
+        private event Action<Player> OnHandChanged;
+
+        public Player(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddCard(Card card)
+        {
+            _hand.Add(card);
+            OnHandChanged?.Invoke(this);
+        }
+
+        public bool PlayCard(Card card)
+        {
+            if (_hand.Contains(card))
+            {
+                _hand.Remove(card);
+                OnHandChanged?.Invoke(this);
+                return true;
+            }
+            return false;
+        }
+        public IReadOnlyList<Card> Hand => _hand.AsReadOnly();
+
+        public int CardCount => _hand.Count;
+
+        public bool IsOut() => _hand.Count == 0;
+    }
+}
